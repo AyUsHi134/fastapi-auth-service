7090d7b838d5b5159d0bd2e6bfd5caf1004c3e26
Ayushi Singh <singhayushi9369@gmail.com>
2026-05-12 02:59:56 +0530

    Added delete post functionality with 204 responses

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI , HTTPException , status    
+from fastapi import FastAPI , HTTPException , status , Response  
 from pydantic import BaseModel
 from random import randrange
 
@@ -18,6 +18,11 @@ def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get("/")
 async def root():
@@ -41,3 +46,12 @@ def get_post(id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail= f"post with id: {id} was not found")
     return{"post_detail": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT) 
